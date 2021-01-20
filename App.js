e2e8d47477266e8ef4d0d4f6442169ce459fd041
RadiantF-pondok <radiantfadilah0@gmail.com>
2021-01-20 17:29:04 +0700

    trying picker

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Navigator from './src/router/Navigator';
 import {Provider} from 'react-redux';
 import {store} from './src/redux/store/index';
+
 export default class App extends Component {
   render() {
     return (
